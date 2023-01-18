bf86bb02fbfeb08eda0ab98e6c5b7c29bce44a62
sereja-artemov <artemov46@gmail.com>
2023-01-18 17:22:09 +0300

    features: Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,21 @@
 import s from './Header.module.scss';
+import Link from 'next/link';
 
 export default function Header() {
     return (
-        <header className={s.word}>
-            asd
+        <header className={s.header}>
+            <div className="container-fluid">
+                <a href="" className="logo"><img src="" alt="" className="logo-image"/></a>
+                <nav className={s.nav}>
+                    <ul className={s.list}>
+                        <li className={s.listItem}><Link href="/" className={s.link}>Главная</Link></li>
+                        <li className={s.listItem}><Link href="/about" className={s.link}>Обо мне</Link></li>
+                        <li className={s.listItem}><Link href="#" className={s.link}>Портфолио</Link></li>
+                        <li className={s.listItem}><Link href="#" className={s.link}>Блог</Link></li>
+                        <li className={s.listItem}><Link href="#" className={s.link}>Услуги</Link></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
