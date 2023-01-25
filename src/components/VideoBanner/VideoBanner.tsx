f0392453b73108181406888c969b7fd86f675376
sereja-artemov <artemov46@gmail.com>
2023-01-25 13:14:42 +0300

    features: Добавить адаптивность header. Реализовать функционал всплывающего окна меню

--- src/components/VideoBanner/VideoBanner.tsx
+++ src/components/VideoBanner/VideoBanner.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import s from '../VideoBanner/VideoBanner.module.scss';
-import posterImage from '../../images/poster-image.jpg';
 
 export default function VideoBanner() {
     return (
         <div className={`${s.wrapper} container block-mb`}>
-            <video className={s.video} controls
-                   preload="none"
+            <video className={s.video}
                    loop
                    autoPlay
                    muted
-                   poster={posterImage}
+                   poster={'/banner-video-poster.jpg'}
                    // controls={false}
             >
-                <source src={"../../video/banner-video.mp4"} type="video/mp4" />
+                <source src={"/video/banner-video.mp4"} type="video/mp4" />
             </video>
             <button className={s.resumeBtn}>Резюме</button>
         </div>
