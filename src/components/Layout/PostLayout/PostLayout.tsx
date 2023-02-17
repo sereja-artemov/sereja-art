ab8d5dcfb99201bb5980f3da33ca5dea097f379e
sereja-artemov <artemov46@gmail.com>
2023-02-17 14:09:34 +0300

    features: окончания в количестве слов PostLayout

--- src/components/Layout/PostLayout/PostLayout.tsx
+++ src/components/Layout/PostLayout/PostLayout.tsx
@@ -1,13 +1,14 @@
 import s from './PostLayout.module.scss';
 import getLocaleDate from "@/lib/getLocaleDate";
 import {AiOutlineCalendar, AiOutlineFieldTime, AiOutlineRead} from "react-icons/ai";
+import getWordEnding from "@/lib/getWordEnding";
 
 
 function PostLayout({post, children}) {
   console.log(post)
 
   return (
-    <section className={s.postLayout}>
+    <section className={`${s.postLayout} container-fluid`}>
 
       <article className={s.post}>
         <div className={s.topBlock}>
@@ -23,7 +24,8 @@ function PostLayout({post, children}) {
               </span>
               <span className={s.words}>
                 {<AiOutlineRead className={`${s.icon} ${s.iconRead}`} />}
-                {post.meta.readingTime.words} слов</span>
+                {post.meta.readingTime.words}
+                {getWordEnding(post.meta.readingTime.words, [' слово', ' слова', ' слов'])}</span>
             </div>
           </div>
         </div>
