dffab64aa8bb8218098f4bfdbb04bd0be4236b78
sereja-art <sereja.artemov.art@yandex.ru>
2023-02-25 22:44:25 +0300

    refactor: ProjectsPage.tsx

--- src/components/ProjectsPage/ProjectsPage.tsx
+++ src/components/ProjectsPage/ProjectsPage.tsx
@@ -2,16 +2,22 @@ import s from './ProjectsPage.module.scss';
 import {project} from "@/data/projectsData";
 import {projectsProps} from "@/pages/projects";
 import ProjectCardItem from "@/components/ProjectCardItem/ProjectCardItem";
+import PageTopBlock from "@/components/PageTopBlock/PageTopBlock";
 
 const ProjectsPage = ({projects}:projectsProps) => {
   const projectsArr: project[] = JSON.parse(projects);
 
   return (
-    <section className={`${s.projects} container-fluid`}>
-      {projectsArr.map((project, index) => {
-        return <ProjectCardItem key={index} data={project}/>
-      })
-      }
+    <section className={`container-fluid`}>
+      <PageTopBlock pageTitle="Проекты">
+        За годы работы я выполнил более 2000 задач, вот некоторые из них.
+      </PageTopBlock>
+      <div className={s.projectItems}>
+        {projectsArr.map((project, index) => {
+          return <ProjectCardItem key={index} data={project}/>
+        })
+        }
+      </div>
     </section>
   );
 };
