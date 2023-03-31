62e252f9b6b15f3c83c7df14e867cb3c15836271
sereja-artemov <artemov46@gmail.com>
2023-03-31 17:25:49 +0300

    refactor

--- src/components/ContactsPage/ContactsPage.tsx
+++ src/components/ContactsPage/ContactsPage.tsx
@@ -11,15 +11,18 @@ const ContactsPage = () => {
       <a href='mailto:artemov46@gmail.com' className={`${s.textBlock} ${s.contactsItem}`}>artemov46@gmail.com</a>
       <p className={`${s.textBlock}`}>или в любом мессенджере</p>
       <div className={`${s.textBlock}`}>
-        <span className={s.contactsItem}>+7 951 333 99 23</span>
-        <div className={s.messengerItems}>
-          <Link href='https://t.me/sereja_art' target='_blank'>— Telegram</Link>
-          <Link href='https://wa.me/79513339923' target='_blank'>— WhatsApp</Link>
-        </div>
+        <span className={`${s.contactsItem} ${s.messengerItems}`}>
+          <Link href='https://t.me/sereja_art' target='_blank'>Telegram</Link>
+          <Link href='https://wa.me/79513339923' target='_blank'>WhatsApp</Link>
+        </span>
+        {/*<div className={s.messengerItems}>*/}
+        {/*  <Link href='https://t.me/sereja_art' target='_blank'>— Telegram</Link>*/}
+        {/*  <Link href='https://wa.me/79513339923' target='_blank'>— WhatsApp</Link>*/}
+        {/*</div>*/}
       </div>
       <p className={`${s.textBlock}`}>я в соцсетях</p>
       <div className={s.messengerItems}>
-        <span className={s.contactsItem}>ВКонтакте</span>
+        <Link href='https://vk.com/sereja_art' target='_blank' className={s.contactsItem}>ВКонтакте</Link>
       </div>
     </section>
   );
