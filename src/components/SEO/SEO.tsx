776c5acb591cca0d8f2046daad8aa4434fb32903
sereja-artemov <artemov46@gmail.com>
2023-04-19 16:13:54 +0300

    add favicon

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -1,5 +1,5 @@
-import Head from "next/head";
-import useWindowLocation from "@/hooks/useWindowLocation";
+import Head from 'next/head';
+import useWindowLocation from '@/hooks/useWindowLocation';
 
 interface SEOProps {
   title?: string,
@@ -8,37 +8,40 @@ interface SEOProps {
   previewImage?: string,
 }
 
-const SEO = ({ title, description, keywords, previewImage }:SEOProps) => {
+const SEO = ({ title, description, keywords, previewImage }: SEOProps) => {
   const { currentURL } = useWindowLocation();
 
   return (
     <Head>
-      <meta charSet="utf-8" />
+      <meta charSet='utf-8' />
       <meta
-        name="viewport"
-        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        name='viewport'
+        content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
       />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="description" content={description || "sereja-art"} />
-      <title>{`${title || ""} — sereja-art`}</title>
-      <meta name="theme-color" content="#000" />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link rel="manifest" href="/manifest.json" />
-      <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
-      <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
-      <meta name="author" content="Sergey Artemov"></meta>
-      <meta name="robots" content="index,follow" />
+      <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+      <meta name='description' content={description || 'sereja-art'} />
+      <title>{`${title || ''} — sereja-art`}</title>
+      <link rel='apple-touch-icon' sizes='180x180' href='public/apple-touch-icon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='public/favicon-32x32.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='public/favicon-16x16.png' />
+      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+      <meta name='msapplication-TileColor' content='#da532c' />
+      <meta name='theme-color' content='#ffffff' />
+      <link rel='manifest' href='/manifest.json' />
+      <meta httpEquiv='Content-Type' content='text/html; charset=UTF-8' />
+      <meta name='author' content='Sergey Artemov'></meta>
+      <meta name='robots' content='index,follow' />
       <meta
-        name="keywords"
-        content={`${keywords || ""} sereja-art, sergey artemov, сайт портфолио веб-разработчкика, веб разработка, frontend разработчик, портфолио веб разработчика`}
+        name='keywords'
+        content={`${keywords || ''} sereja-art, sergey artemov, сайт портфолио веб-разработчкика, веб разработка, frontend разработчик, портфолио веб разработчика`}
       />
 
       {/* Og */}
-      <meta property="og:title" content={`${title || ""} sereja-art`} />
-      <meta property="og:description" content={description || "sereja-art"} />
-      <meta property="og:site_name" content="sereja-art" />
-      <meta property="og:url" content={currentURL} key="ogurl" />
-      <meta property="og:image" content={previewImage || ""} />
+      <meta property='og:title' content={`${title || ''} sereja-art`} />
+      <meta property='og:description' content={description || 'sereja-art'} />
+      <meta property='og:site_name' content='sereja-art' />
+      <meta property='og:url' content={currentURL} key='ogurl' />
+      <meta property='og:image' content={previewImage || ''} />
 
     </Head>
   );
